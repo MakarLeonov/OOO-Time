3c5ad5321cfc77722fbb60a54d47cc1422e6a180
Makar Leonov <MakarLeonov>
2023-06-09 23:59:50 +0300

    Did the adaptation of the static service info for the mobile devices

--- frontend/src/components/ServicePage/ServiceInfo/DynamicServiceInfo.vue
+++ frontend/src/components/ServicePage/ServiceInfo/DynamicServiceInfo.vue
@@ -57,6 +57,7 @@ export default {
         font-weight: 400;
         font-size: 20px;
         margin-top: 10px;
+        text-align: justify;
 
         color: #494949;
     }
@@ -71,6 +72,87 @@ export default {
         color: #494949;
 
         transition: all .2s ease;
+    }
+
+    @media (max-width: 1300px) {
+        .service_title {
+            font-size: 34px;
+            line-height: 46px;
+        }
+        .description {
+            font-size: 19px;
+        }
+        .cost {
+            font-size: 22px;
+        }
+    }
+
+    @media (max-width: 650px) {
+        .service_title {
+            line-height: 40px;
+        }
+        .description {
+            margin-top: 5px;
+        }
+        .cost {
+            font-size: 22px;
+        }
+    }
+
+    @media (max-width: 500px) {
+        .service_title {
+            line-height: 40px;
+            font-size: 31px;
+        }
+        .description {
+            margin-top: 5px;
+            font-size: 19px;
+        }
+    }
+
+    @media (max-width: 450px) {
+        .service_title {
+            font-size: 29px;
+        }
+        .description {
+            margin-top: 5px;
+            font-size: 19px;
+        }
+        .cost {
+            font-size: 22px;
+        }
+    }
+
+    @media (max-width: 389px) {
+        .service_title {
+            font-size: 27px;
+        }
+    }
+
+    @media (max-width: 360px) {
+        .service_title {
+            font-size: 26px;
+        }
+    }
+
+    @media (max-width: 350px) {
+        .service_title {
+            font-size: 26px;
+        }
+        .description {
+            margin-top: 5px;
+            font-size: 18px;
+        }
+    }
+
+    @media (max-width: 340px) {
+        .service_title {
+            font-size: 25px;
+        }
+        .description {
+            margin-top: 5px;
+            font-size: 18px;
+        }
     }
 
 </style>
--- frontend/src/components/ServicePage/ServiceInfo/StaticInfo.vue
+++ frontend/src/components/ServicePage/ServiceInfo/StaticInfo.vue
@@ -1,13 +1,50 @@
 <template>
-    <div>
-        Какая-то статическая информация
-    </div>
+    <section>
+        <!-- <p class="service_title">Титульник</p> -->
+        <p class="description">
+            Все услуги, предоставляемые нашим автосервисом, поделены на категории по виду неисправности. 
+            Чтобы открыть подробную информацию об услуге достаточно нажать вид неисправности и бырать интересующую вас услугу.
+        </p>
+    </section>
 </template>
 <script>
 export default {
     
 }
 </script>
-<style lang="">
-    
+<style lang="scss" scoped>
+    .service_title {
+        
+        font-family: 'Rubik';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 35px;
+        line-height: 57px;
+        display: flex;
+        align-items: center;
+
+        color: #000000;
+    }
+    .description {
+        font-family: 'Rubik';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 20px;
+        margin-top: 10px;
+        text-align: justify;
+
+        color: #494949;
+    }
+    .cost {
+        font-family: 'Rubik';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 23px;
+        line-height: 20px;
+        margin-top: 15px;
+
+        color: #494949;
+
+        transition: all .2s ease;
+    }
 </style>
